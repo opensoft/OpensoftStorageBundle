3ce408a27060894f0199d3bb6497c9812f32dc6c
Richard Fullmer <richardfullmer@gmail.com>
2017-06-21 15:22:10 -0700

    Fix missing use statement

--- Entity/Storage.php
+++ Entity/Storage.php
@@ -5,6 +5,7 @@ namespace Opensoft\StorageBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Opensoft\StorageBundle\Storage\Adapter\AwsS3AdapterConfiguration;
 use Opensoft\StorageBundle\Storage\Adapter\LocalAdapterConfiguration;
 
 /**
